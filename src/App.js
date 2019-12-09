04b856595d022ec2f164071dee6ef956e2240013
pekkalei <47647279+pekkalei@users.noreply.github.com>
2019-12-09 17:45:06 +0200

    update
    we collect your data, ty

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostForm from './components/PostForm'
 import LoginForm from './components/LoginForm'
 import Footer from './components/Footer'
 import LogoutForm from './components/LogoutForm'
+import CookieConsent from 'react-cookie-consent'
 
 const App = props => {
 	useEffect(() => {
@@ -24,13 +25,26 @@ const App = props => {
 			{props.auth.username === null ? <LoginForm /> : <LogoutForm />}
 			<br></br>
 			<br></br>
-			<img src={require('./images/logo.png')} className='' alt='kuva' width='100%'></img>
+			<img
+				src={require('./images/logo.png')}
+				className=''
+				alt='kuva'
+				width='100%'
+			></img>
 			<div className='mx-auto'>
 				{props.auth.username === null ? <LoginForm /> : <PostForm />}
 				<h1>Posts</h1>
 			</div>
 			<PostList />
 			<div className='row justify-content-center'></div>
+			<CookieConsent
+				location='bottom'
+				buttonText='Yes, I understand'
+				buttonStyle={{ background: '#bb86fc', color: 'white' }}
+			>
+				In order to optimize the website and to continuously improve ASMR, we
+				collect your data.{' '}
+			</CookieConsent>
 			<Footer />
 		</div>
 	)
